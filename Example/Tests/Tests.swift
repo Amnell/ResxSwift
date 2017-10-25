e508682ad187e8445bc82d371b70c3c2946b25b3
Mathias Amnell <mathias@apping.se>
2017-10-25 16:13:37 +0200

    Add more tests

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -14,32 +14,41 @@ class TableOfContentsSpec: QuickSpec {
         let fileURL_en = URL(fileURLWithPath: filePath_en)
         let fileURL_sv = URL(fileURLWithPath: filePath_sv)
         
-        describe("these will fail") {
-
-            context("these will pass") {
-
-                it("can load resource") {
-                    let resx = ResxSwift()
-                    try! resx.load(fileURL: URL.init(fileURLWithPath: filePath_en), language: "en")
-                    expect(resx.localizedString("Exception_Authorization_Username_Not_Found", language: "en")) == "User not exists."
-                }
-                
-                it("can load multiple languages") {
-                    let resx = ResxSwift()
-                    try! resx.load(fileURL: fileURL_en, language: "en")
-                    try! resx.load(fileURL: fileURL_sv, language: "sv")
-                    
-                    expect(resx.localizedString("Exception_Authorization_Username_Not_Found", language: "en")) == "User not exists."
-                    expect(resx.localizedString("Exception_Authorization_Username_Not_Found", language: "sv")) == "Användaren finns inte."
-                }
-                
-                it("will fallback to key if value not found") {
-                    let resx = ResxSwift()
-                    try! resx.load(fileURL: fileURL_en, language: "en")
-                    
-                    expect(resx.localizedString("🍌", language: "en")) == "🍌"
-                }
-            }
+        it("can load resource") {
+            let resx = ResxSwift()
+            try! resx.load(fileURL: URL.init(fileURLWithPath: filePath_en), language: "en")
+            expect(resx.localizedString("Exception_Authorization_Username_Not_Found", language: "en")) == "User not exists."
         }
+        
+        it("can load multiple languages") {
+            let resx = ResxSwift()
+            try! resx.load(fileURL: fileURL_en, language: "en")
+            try! resx.load(fileURL: fileURL_sv, language: "sv")
+            
+            expect(resx.localizedString("Exception_Authorization_Username_Not_Found", language: "en")) == "User not exists."
+            expect(resx.localizedString("Exception_Authorization_Username_Not_Found", language: "sv")) == "Användaren finns inte."
+        }
+        
+        it("will fallback to key if value not found") {
+            let resx = ResxSwift()
+            try! resx.load(fileURL: fileURL_en, language: "en")
+            
+            expect(resx.localizedString("🍌", language: "en")) == "🍌"
+        }
+        
+        it("infers namespace") {
+            let resx = ResxSwift()
+            try! resx.load(fileURL: URL.init(fileURLWithPath: filePath_en), language: "en")
+            expect(resx.defaultNamespace) == "Resource"
+        }
+        
+        it("supports multiple namespaces") {
+            let resx = ResxSwift()
+            try! resx.load(fileURL: URL.init(fileURLWithPath: filePath_en), language: "en", namespace: "x")
+            try! resx.load(fileURL: URL.init(fileURLWithPath: filePath_en), language: "en", namespace: "y")
+            expect(resx.localizedString("Exception_Authorization_Username_Not_Found", namespace: "x", language: "en")) == "User not exists."
+            expect(resx.localizedString("Exception_Authorization_Username_Not_Found", namespace: "y", language: "en")) == "User not exists."
+        }
+        
     }
 }
